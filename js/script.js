27bd35637732091bd8583a47d7615e97bc90339f
Mathieu Desilets <mathieu.desilets@gmail.com>
2020-12-07 15:07:18 -0500

    created showPage function

--- js/script.js
+++ js/script.js
@@ -11,13 +11,34 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
-
-
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
 
+function showPage(data, page) {
+   const startIndex = (page * 9) - 9;
+   const endIndex = page * 9;
+   const ul = document.querySelector('ul');
+   
+   for ( let i = 0; i < data.length; i++ ) {
+      if ( i >= startIndex && i < endIndex ) {
+         const li = document.createElement('li');
+         const divDetail = document.createElement('div');
+         const divJoined = document.createElement('div');
+         li.className = "student-item cf";
+         divDetail.className = "student-details";
+         divDetail.innerHTML = `<img class="avatar" src="${data[i]['picture']['medium']}" alt="Profile Picture">
+                               <h3>${data[i]['name']['first']} ${data[i]['name']['last']}</h3>
+                               <span class="email">${data[i]['email']}</span>`;
+         divJoined.className = "joined-details"
+         divJoined.innerHTML = `<span class="date">Joined ${data[i]['registered']['date']}</span>`;
+         li.appendChild(divDetail);
+         li.appendChild(divJoined);
+         ul.appendChild(li); 
+      }
+   }
+}
 
 
 /*
@@ -28,3 +49,4 @@ This function will create and insert/append the elements needed for the paginati
 
 
 // Call functions
+showPage(data, 1);
